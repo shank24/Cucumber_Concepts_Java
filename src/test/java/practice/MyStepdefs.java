1cac2fb29c63eea530c8a655f9ce4bbf69d79c54
shankkalra <shankkalra@gmail.com>
2022-02-10 01:05:09 +0530

    Added Code

---
+++ src/test/java/practice/MyStepdefs.java
@@ -0,0 +1,27 @@
+package practice;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+
+    @Given("^I am dummy$")
+    public void iAmDummy() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("^I do dummy things$")
+    public void iDoDummyThings() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("^dummy things happen$")
+    public void dummyThingsHappen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/java/practice/dummy.feature
+++ src/test/java/practice/dummy.feature
@@ -1,26 +1,5 @@
-
-@dummyfeatures
-
-  Feature: Add to Cart
-
-    As a customer of xyz app
-    I want to add a product to the cart
-    So that I can purchase the product
-
-    Rule: rule1
-    Background: This is a background
-
-    Background: This is a background
-
-      Scenario:Dummy Scenario
-        Given : I'm dummy
-        When : I do dummy things
-        Then : dummy things happen
-
-    Rule: rule2
-    Background: This is a background
-
-      Scenario:Dummy Scenario
-        Given : I'm dummy
-        When : I do dummy things
-        Then : dummy things happen
+Feature:
+  Scenario: Dummy Scenario
+    Given I am dummy
+    When I do dummy things
+    Then dummy things happen
