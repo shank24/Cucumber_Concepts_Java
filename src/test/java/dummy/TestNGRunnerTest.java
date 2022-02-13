78514880332d741e22f5b6ff5b5ef8cd4d908eb3
shankkalra <shankkalra@gmail.com>
2022-02-13 19:32:21 +0530

    Cucumber Snippets

--- src/test/java/dummy/TestNGRunnerTest.java
+++ src/test/java/dummy/TestNGRunnerTest.java
@@ -4,9 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
 
 @CucumberOptions(
-        plugin =  {"pretty", "html:target/cucumber.html"}
+        plugin =  {"pretty", "html:target/cucumber.html", "summary"},
+        snippets = CAMELCASE
 )
 public class TestNGRunnerTest extends AbstractTestNGCucumberTests {
 
