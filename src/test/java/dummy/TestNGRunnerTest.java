d49782e469313a00b4f15b2dd4a385aa93c4de69
shankkalra <shankkalra@gmail.com>
2022-02-13 17:25:27 +0530

    Test NG Runner

--- src/test/java/dummy/TestNGRunnerTest.java
+++ src/test/java/dummy/TestNGRunnerTest.java
@@ -2,7 +2,20 @@ package dummy;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 @CucumberOptions(plugin = "message:target/cucumber-report.ndjson")
 public class TestNGRunnerTest extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    public void beforeClass(){
+    System.out.println("In Before Class");
+    }
+
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("In After Class");
+    }
 }
