ec40cd3e6d1b0948a4bc8ac785e2b0d34ae25872
shankkalra <shankkalra@gmail.com>
2022-02-15 21:58:23 +0530

    Data Table

--- src/test/java/dummy/objects/Product.java
+++ src/test/java/dummy/objects/Product.java
@@ -5,15 +5,15 @@ public class Product {
 
     private String name;
 
+    public Product(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public Product(String name) {
         this.name = name;
     }
 }
---
+++ src/test/resources/dummy/data_table.feature
@@ -0,0 +1,11 @@
+Feature: DataTable Demo
+
+  Rule: Values from DataTable
+
+    Scenario: Data Table
+      Given my credentials
+      | john | john123 |
+
+
+
+
