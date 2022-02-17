4384124010ddc517bfc31def117c563c856ac233
shankkalra <shankkalra@gmail.com>
2022-02-17 18:41:42 +0530

    Hooks Methods

--- src/test/java/dummy/hooks/CucumberHooks.java
+++ src/test/java/dummy/hooks/CucumberHooks.java
@@ -22,19 +22,22 @@ public class CucumberHooks {
     @Before
     public void before(Scenario scenario){
         System.out.println("Before Hook");
+        scenarioInfo(scenario);
+    }
+
+    //Executed before each of the example listed in the Scenario Outline
+    @After
+    public void after(Scenario scenario){
+        System.out.println("After Hook");
+        scenarioInfo(scenario);
+    }
+
+    private void scenarioInfo(Scenario scenario) {
         System.out.println("SCENARIO NAME -> " + scenario.getName());
         System.out.println("SCENARIO LINE -> " + scenario.getLine());
         System.out.println("SCENARIO TAGS -> " + scenario.getSourceTagNames());
         System.out.println("SCENARIO STATUS -> " + scenario.getStatus());
         System.out.println("SCENARIO URI -> " + scenario.getUri());
         System.out.println("SCENARIO IsFailed -> " + scenario.isFailed() + "\n");
-
-    }
-
-
-    //Executed before each of the example listed in the Scenario Outline
-    @After
-    public void after(Scenario scenario){
-        System.out.println("After Hook");
     }
 }
